4b427c1dd70aab5e8162c8d5dc30e346203a67b2
Stefan Malinović <malina.stefan39@gmail.com>
2023-09-06 22:24:31 +0200

    Ispravke

--- ProjekatGrafovi/MainWindow.xaml.cs
+++ ProjekatGrafovi/MainWindow.xaml.cs
@@ -1,24 +1,11 @@
 ﻿using Microsoft.Win32;
-using QuickGraph;
-using QuickGraph.Serialization;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Markup;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml.Linq;
 
 namespace ProjekatGrafovi
 {
